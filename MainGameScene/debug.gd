9763843ad22ed20bd421e878576af8cfea03cd5a
Starkandco <Starkandco@hotmail.com>
2024-02-10 14:52:59 +0000

    Final commit

--- MainGameScene/debug.gd
+++ MainGameScene/debug.gd
@@ -14,7 +14,7 @@ func _on_button_pressed():
 	count += 1
 
 func _on_option_button_item_selected(index):
-	match index:
+	match int(index):
 		0:
 			$"../Timer".stop()
 			$"../Timer".wait_time = 2
@@ -41,6 +41,24 @@ func _on_option_button_item_selected(index):
 			if get_tree().get_first_node_in_group("Main").players.size() > 0:
 				$"../Timer".start()
 
+
+func _on_option_button_2_item_selected(index):
+	match int(index):
+		0:
+			get_tree().get_first_node_in_group("Main").spawn = 1
+			get_tree().get_first_node_in_group("Main").spawn_counter = 1
+		1:
+			get_tree().get_first_node_in_group("Main").spawn = 2
+			get_tree().get_first_node_in_group("Main").spawn_counter = 2
+		2:
+			get_tree().get_first_node_in_group("Main").spawn = 4
+			get_tree().get_first_node_in_group("Main").spawn_counter = 4
+		3:
+			get_tree().get_first_node_in_group("Main").spawn = 6
+			get_tree().get_first_node_in_group("Main").spawn_counter = 6
+		4:
+			get_tree().get_first_node_in_group("Main").spawn = 9
+			get_tree().get_first_node_in_group("Main").spawn_counter = 9
 
 func _on_close_requested():
 	visible = false
